b6b4e06a7f13d26779625ac188d0030a895aa04f
Raptus <lee@familiehartwich.de>
2020-10-19 18:06:17 +0200

    Initial commit

---
+++ app/src/main/java/com/raptus/weathertotorial/ui/weather/future/list/FutureListViewModel.kt
@@ -0,0 +1,7 @@
+package com.raptus.weathertotorial.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
